676f1e453d621af9101f74c93d33a42fac350438
pcorcole <pablo.corcoles-molina@capgemini.com>
2022-06-01 10:53:07 +0200

    Create tests

--- Devon4Net.Test/AccessCodeServiceTests.cs
+++ Devon4Net.Test/AccessCodeServiceTests.cs
@@ -39,19 +39,6 @@ namespace Devon4Net.Test
             Assert.Equal(expectedAccessCodeList[0].Id, resultList[0].Id);
         }
 
-        public List<AccessCode> GetQueueWith()
-        {
-            var expectedAccessCodeList = new List<AccessCode>();
-            for(int i=0; i<100; i++)
-            {
-                expectedAccessCodeList.Add(new AccessCode()
-                {
-                }
-                );
-            };
-            return expectedAccessCodeList;
-        }
-
         [Fact]
         public async Task CreateFirstAccessCodeOK()
         {
@@ -71,12 +58,79 @@ namespace Devon4Net.Test
                 .ReturnsAsync(expectedAClist[0]);
             mockVisitorRepository.Setup(repository => repository.Create(It.IsAny<Guid>()))
                .ReturnsAsync(expectedVisitor);
+            mockAccessCodeRepository.Setup(repository => repository.GetAccessCode(It.IsAny<Expression<Func<AccessCode, bool>>>()))
+                .ReturnsAsync(new List<AccessCode>());
             //Act
             var result = await CreateAccessCodeServiceTest().CreateAccessCode(expectedQueue.Id).ConfigureAwait(false);
 
             //Assert
-            Assert.Equal(expectedAClist[0].Id, result.Id); //Q001 should be the result
+            Assert.Equal(expectedAClist[0].Code, result.Code); //Q001 should be the result
         }
+
+        [Fact]
+        public async Task CreateSecondAccessCodeOK()
+        {
+            //Arrange
+            var expectedVisitor = CreateMockData.CreateVisitor();
+            var expectedQueue = CreateMockData.CreateQueue();
+            var expectedAClist = CreateMockData.GetNListOfAC(1);
+            var expectedResultAC = CreateMockData.CreateAccessCode(2);
+            expectedAClist[0].QueueId = expectedQueue.Id;
+            expectedAClist[0].VisitorUid = expectedVisitor.Uid;
+            
+
+            mockAccessCodeRepository.Setup(repository => repository.Create(
+                It.IsAny<string>(),
+                It.IsAny<TimeSpan?>(),
+                It.IsAny<TimeSpan?>(),
+                It.IsAny<Status_t>(),
+                It.IsAny<Guid>(),
+                It.IsAny<int>()
+                ))
+                .ReturnsAsync(expectedResultAC);
+            mockVisitorRepository.Setup(repository => repository.Create(It.IsAny<Guid>()))
+               .ReturnsAsync(expectedVisitor);
+            
+            mockAccessCodeRepository.Setup(repository => repository.GetAccessCode(It.IsAny<Expression<Func<AccessCode, bool>>>()))
+                .ReturnsAsync(expectedAClist);
+            //Act
+            var result = await CreateAccessCodeServiceTest().CreateAccessCode(expectedQueue.Id).ConfigureAwait(false);
+
+            //Assert
+            Assert.Equal(expectedResultAC.Code, result.Code); //Q002 should be the result
+        }
+
+        [Fact]
+        public async Task CreateFirstFromFullCodeOK()
+        {
+            //Arrange
+            var expectedVisitor = CreateMockData.CreateVisitor();
+            var expectedQueue = CreateMockData.GetFullQueue();
+            var expectedAClist = CreateMockData.GetFullListOfACInQueue();
+            var expectedResultAC = CreateMockData.CreateAccessCode(1);
+            expectedAClist[0].QueueId = expectedQueue.Id;
+            expectedAClist[0].VisitorUid = expectedVisitor.Uid;
+
+            mockAccessCodeRepository.Setup(repository => repository.Create(
+                It.IsAny<string>(),
+                It.IsAny<TimeSpan?>(),
+                It.IsAny<TimeSpan?>(),
+                It.IsAny<Status_t>(),
+                It.IsAny<Guid>(),
+                It.IsAny<int>()
+                ))
+                .ReturnsAsync(expectedResultAC);
+            mockVisitorRepository.Setup(repository => repository.Create(It.IsAny<Guid>()))
+               .ReturnsAsync(expectedVisitor);
+            mockAccessCodeRepository.Setup(repository => repository.GetAccessCode(It.IsAny<Expression<Func<AccessCode, bool>>>()))
+                .ReturnsAsync(expectedAClist);
+            //Act
+            var result = await CreateAccessCodeServiceTest().CreateAccessCode(expectedQueue.Id).ConfigureAwait(false);
+
+            //Assert
+            Assert.Equal("Q001", result.Code); //Q002 should be the result
+        }
+
     }
 
     public class Base
--- Devon4Net.Test/Fixtures/CreateMockData.cs
+++ Devon4Net.Test/Fixtures/CreateMockData.cs
@@ -27,7 +27,29 @@ namespace Devon4Net.Test.Fixtures
                     Id = 1,
                     StartTime = null,
                     Status = Status_t.notStarted,
+                },
+                new AccessCode()
+                {
+                    Code="Q002",
+                    Createdtime = new TimeSpan(0, 14, 11),
+                    Endtime = new TimeSpan(0, 14, 11),
+                    Id = 2,
+                    StartTime = null,
+                    Status = Status_t.notStarted,
                 }
+            };
+        }
+
+        public static AccessCode CreateAccessCode(int code)
+        {
+            return new AccessCode()
+            {
+                Code = "Q" + code.ToString("000"),
+                Createdtime = new TimeSpan(0, 14, 11),
+                Endtime = new TimeSpan(0, 14, 11),
+                Id = code,
+                StartTime = null,
+                Status = Status_t.notStarted,
             };
         }
 
@@ -41,6 +63,58 @@ namespace Devon4Net.Test.Fixtures
                 Started = false,
                 
             };
+        }
+
+        public static List<AccessCode> GetNListOfAC(int n)
+        {
+            var expectedAccessCodeList = new List<AccessCode>();
+            for (int i = 1; i <= n; i++)
+            {
+                expectedAccessCodeList.Add(new AccessCode()
+                {
+                    Code = "Q" + i.ToString("000"),
+                    Id = i,
+                });
+            }
+            return expectedAccessCodeList;
+        }
+
+        public static List<AccessCode> GetFullListOfAC()
+        {
+            var expectedAccessCodeList = new List<AccessCode>();
+            for (int i = 1; i < 1000; i++)
+            {
+                expectedAccessCodeList.Add(new AccessCode()
+                {
+                    Code = "Q" + i.ToString("000"),
+                    Id = i,
+                });
+            }
+            return expectedAccessCodeList;
+        }
+
+        public static Queue GetFullQueue()
+        {
+            var expectedAccessCodeList = GetFullListOfAC();
+            var expectedQueue = CreateMockData.CreateQueue();
+            foreach(AccessCode ac in expectedAccessCodeList)
+            {
+                ac.QueueId = expectedQueue.Id;
+            }
+            return expectedQueue;
+        }
+
+        public static List<AccessCode> GetFullListOfACInQueue()
+        {
+            var expectedAccessCodeList = GetFullListOfAC();
+            var expectedQueue = CreateMockData.CreateQueue();
+            var aclist = new List<AccessCode>();
+            foreach (AccessCode ac in expectedAccessCodeList)
+            {
+                ac.QueueId = expectedQueue.Id;
+                aclist.Add(ac);
+            }
+            return aclist;
         }
     }
 }
